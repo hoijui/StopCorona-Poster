bac7cf4071f07562a9e8760dd0f7e84f417f81c2
hoijui <hoijui.quaero@gmail.com>
2020-04-07 16:19:34 +0200

    generate an index.html linking to all the generated documents

--- generate.sh
+++ generate.sh
@@ -33,5 +33,36 @@ do
 		--export-plain-svg "$output_dir/$poster.svg"
 done
 
+# Create an HTML index that links to all the generated image files
+cd "$output_dir"
+idx="index.html"
+echo "<html>" > "$idx"
+echo "<head>" >> "$idx"
+echo "<title>StopCorona-Poster (generated files)</title>" >> "$idx"
+echo "</head>" >> "$idx"
+echo "<body>" >> "$idx"
+echo "<h1>StopCorona-Poster (generated files)</h1>" >> "$idx"
+echo "<p>This output is generated after each commit to <a href=\"https://gitlab.com/opencultureagency/StopCorona-Poster\">the StopCorona-Poster repo</a></p>" >> "$idx"
+echo "<h3>as PNG bitmap images</h3>" >> "$idx"
+echo "<ul>" >> "$idx"
+for file in *.png; do
+  echo "<li><a href=\"$file\">$file - <img src=\"$file\" alt=\"$file\" width=\"320\"/></a></li>" >> "$idx"
+done
+echo "</ul>" >> "$idx"
+echo "<h3>as PDF documents</h3>" >> "$idx"
+echo "<ul>" >> "$idx"
+for file in *.pdf; do
+  echo "<li><a href=\"$file\">$file</a></li>" >> "$idx"
+done
+echo "</ul>" >> "$idx"
+echo "<h3>as SVG vector images</h3>" >> "$idx"
+echo "<ul>" >> "$idx"
+for file in *.svg; do
+  echo "<li><a href=\"$file\">$file - <img src=\"$file\" alt=\"$file\" width=\"320\"/></a></li>" >> "$idx"
+done
+echo "</ul>" >> "$idx"
+echo "</body>" >> "$idx"
+echo "</html>" >> "$idx"
+
 echo
 echo "done."
